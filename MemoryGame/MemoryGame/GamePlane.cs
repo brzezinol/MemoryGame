7eaeb936e4cda6d69bef3d3066a60dadbde837f3
brzezinol2 <brzezinol@gmail.com>
2022-10-30 21:12:39 +0100

    Dodaj pliki projektów.

---
+++ MemoryGame/MemoryGame/GamePlane.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MemoryGame
+{
+    public interface IGamePlane
+    {
+        List<IGameTile> Tiles { get; set; }
+        IGameTile T0_0 { get; }
+        IGameTile T0_1 { get; }
+        IGameTile T0_2 { get; }
+        IGameTile T1_0 { get; }
+        IGameTile T1_1 { get; }
+        IGameTile T1_2 { get; }
+        IGameTile T2_0 { get; }
+        IGameTile T2_1 { get; }
+        IGameTile T2_2 { get; }
+
+        Command<IGameTile> TileClick { get; set; }
+    }
+
+    public interface IGameTile
+    {
+        object Char { get; set; }
+        bool IsShown { get; set; }
+        string HiddenChar { get; }
+    }
+
+    public class MemoryCharsGamePlane : IGamePlane
+    {
+
+        public MemoryCharsGamePlane()
+        {
+            Tiles = new List<IGameTile>();
+            TileClick = new Command<IGameTile>(OnTileClick);
+        }
+
+        private void OnTileClick(IGameTile obj)
+        {
+            if (obj == null)
+                return;
+
+            obj.IsShown = !obj.IsShown;
+        }
+
+        public List<IGameTile> Tiles { get; set; }
+
+        public IGameTile T0_0 => Tiles[0 * 3 + 0];
+
+        public IGameTile T0_1 => Tiles[0 * 3 + 1];
+
+        public IGameTile T0_2 => Tiles[0 * 3 + 2];
+
+        public IGameTile T1_0 => Tiles[1 * 3 + 0];
+
+        public IGameTile T1_1 => Tiles[1 * 3 + 1];
+
+        public IGameTile T1_2 => Tiles[1 * 3 + 2];
+
+        public IGameTile T2_0 => Tiles[2 * 3 + 0];
+
+        public IGameTile T2_1 => Tiles[2 * 3 + 1];
+
+        public IGameTile T2_2 => Tiles[2 * 3 + 2];
+
+        public Command<IGameTile> TileClick { get; set; }
+    }
+
+    public class CharGameTileItem : IGameTile, INotifyPropertyChanged
+    {
+        public CharGameTileItem()
+        {
+
+        }
+
+        public CharGameTileItem(string _char)
+        {
+            Char = _char;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public object Char { get; set; }
+
+        bool _isShown = false;
+
+        public bool IsShown
+        {
+            get => _isShown; set
+            {
+                _isShown = value;
+                OnPropertyChanged("IsShown");
+            }
+        }
+
+        public string HiddenChar => "?";
+    }
+}
---
+++ MemoryGame/MemoryGame/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MemoryGame
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
+    }
+
+    public class MainPageViewModel : BaseViewModel
+    {
+        public IGamePlane GamePlane { get; set; }
+
+        public MainPageViewModel()
+        {
+            GamePlane = new MemoryCharsGamePlane();
+            GamePlane.Tiles.Add(new CharGameTileItem("A"));
+            GamePlane.Tiles.Add(new CharGameTileItem("B"));
+            GamePlane.Tiles.Add(new CharGameTileItem("C"));
+            GamePlane.Tiles.Add(new CharGameTileItem("A"));
+            GamePlane.Tiles.Add(new CharGameTileItem("B"));
+            GamePlane.Tiles.Add(new CharGameTileItem("C"));
+            GamePlane.Tiles.Add(new CharGameTileItem("D"));
+            GamePlane.Tiles.Add(new CharGameTileItem("F"));
+            GamePlane.Tiles.Add(new CharGameTileItem("G"));
+        }
+    }
+}
